d7b93245966d839c0c407da0aa52b89de1b82fb1
Akshay Nambiar <akshaychaos@gmail.com>
2023-08-25 01:50:53 +0530

    add docker file

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,20 @@
-WORKDIR .
+FROM golang as builder
+WORKDIR /app
+COPY go.mod ./
+RUN go mod download
+COPY *.go ./
+RUN CGO_ENABLED=0 GOOS=linux go build -o load-test
 
-COPY
+FROM grafana/k6 as loadtest
+WORKDIR /app
+COPY k6-performance-test/ ./
+COPY --from=builder /app .
+RUN pwd
+RUN ls /app
+RUN ls
+RUN cat /etc/os-release
+USER root
+RUN apk update
+RUN apk upgrade
+RUN apk add bash
+#CMD ./load-test
--- server.go
+++ server.go
@@ -1,19 +1,51 @@
 package main
 
 import (
-"fmt"
-"http"
-"log"
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
 )
 
 func main() {
- http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hello!")
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		cmd := exec.Command("k6", "run", `k6-performance-test/main_test.js`)
+		pipe, err := cmd.StdoutPipe()
+		if err != nil {
+			panic(err)
+		}
+		errPipe, err := cmd.StderrPipe()
+		if err != nil {
+			panic(err)
+		}
+		cmd.Start()
 
+		colorRed := "\033[31m"
+		go func() {
+			scanner := bufio.NewScanner(errPipe)
+			scanner.Split(bufio.ScanLines)
+			for scanner.Scan() {
+				m := scanner.Text()
+				fmt.Println(colorRed, m)
+			}
+		}()
 
-      fmt.Printf("Starting server at port 8080\n")
-        if err := http.ListenAndServe(":8080", nil); err != nil {
-            log.Fatal(err)
-        }
+		go func() {
+			scanner := bufio.NewScanner(pipe)
+			scanner.Split(bufio.ScanLines)
+			for scanner.Scan() {
+				m := scanner.Text()
+				fmt.Println(m)
+			}
+		}()
+
+		cmd.Wait()
+		w.Write([]byte("success"))
+	})
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
